31353ce5dc23b81ae1f8892645884db24a978ae7
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-13 21:59:03 +0100

    changed MultiplicationTable and passed test

--- java-loops/src/main/java/dev/rebecamf/loops/MultiplicationTable.java
+++ java-loops/src/main/java/dev/rebecamf/loops/MultiplicationTable.java
@@ -1,13 +1,33 @@
 package dev.rebecamf.loops;
 
-public class MultiplicationTable {
-    public static void main(String[] args) {
+// public class MultiplicationTable {
+//     public static void main(String[] args) {
         
-        int n =  9; 
+//         int n =  9; 
 
-        for (int i =  1; i <=  10; i++) {
+//         for (int i =  1; i <=  10; i++) {
             
-            System.out.println(n + " * " + i + " = " + n * i);
+//             System.out.println(n + " * " + i + " = " + n * i);
+//         }
+//     }
+// }
+
+
+public class MultiplicationTable {
+
+    public static int[] generateTable(int n) {
+        int[] table = new int[10];
+        for (int i =  1; i <=  10; i++) {
+            table[i -  1] = n * i;
+        }
+        return table;
+    }
+
+    public static void main(String[] args) {
+        int n =  9;
+        int[] table = generateTable(n);
+        for (int i =  0; i < table.length; i++) {
+            System.out.println(n + " * " + (i +  1) + " = " + table[i]);
         }
     }
 }
--- java-loops/src/test/java/dev/rebecamf/MultiplicationTableTest.java
+++ java-loops/src/test/java/dev/rebecamf/MultiplicationTableTest.java
@@ -1,5 +1,20 @@
 package dev.rebecamf;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import dev.rebecamf.loops.MultiplicationTable;
+
 public class MultiplicationTableTest {
-    
+
+
+    @Test
+
+    public void testGenerateTable() {
+        int n =  9;
+        int[] expected = {9,  18,  27,  36,  45,  54,  63,  72,  81,  90};
+        int[] result = MultiplicationTable.generateTable(n);
+        assertArrayEquals(expected, result, "The generated table does not match the expected values");
+    }
 }
