3a22207b1da47523cb2979b9005066e0fa221824
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-02-14 21:01:49 +0100

    done all

--- java-loops/src/main/java/dev/rebecamf/loops/MultiplicationTable.java
+++ java-loops/src/main/java/dev/rebecamf/loops/MultiplicationTable.java
@@ -1,16 +1,5 @@
 package dev.rebecamf.loops;
 
-// public class MultiplicationTable {
-//     public static void main(String[] args) {
-        
-//         int n =  9; 
-
-//         for (int i =  1; i <=  10; i++) {
-            
-//             System.out.println(n + " * " + i + " = " + n * i);
-//         }
-//     }
-// }
 
 
 public class MultiplicationTable {
